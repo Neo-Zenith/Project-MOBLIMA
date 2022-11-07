0d46874ee8184d9640981b7010fa12dbe56e8f41
Neo-Zenith <leejuin.office@gmail.com>
2022-11-08 00:41:10 +0800

    banner

--- src/MovieApp.java
+++ src/MovieApp.java
@@ -1,9 +1,25 @@
+import handler.InputHandler;
+import handler.UIHandler;
+import view.MainView;
 import view.MovieAppView;
 
 public class MovieApp {
     
     private static MovieAppView defaultView = new MovieAppView();
     public static void main(String args[]) {
+        UIHandler.clearScreen();
+        System.out.println("╔═══════════════════════════════════════════════════════════════╗");
+        System.out.println("║                                                               ║");
+        System.out.println("║     ╔══╗══╗  ╔════╗  ╔═══╗  ╔      ╔   ╔══╗══╗   ╔════╗       ║");
+        System.out.println("║     ║  ║  ║  ║    ║  ║═══╝  ║      ║   ║  ║  ║   ║    ║       ║");
+        System.out.println("║     ║  ║  ║  ║    ║  ║═══╗  ║      ║   ║  ║  ║   ║════║       ║");
+        System.out.println("║     ╝     ╚  ╚════╝  ╚═══╝  ╚════  ╝   ╝     ╚   ╝    ╚       ║");
+        System.out.println("║                                                               ║");
+        System.out.println("║       MOvie Booking and LIsting Management Application        ║");
+        System.out.println("║                                                               ║");
+        System.out.println("╚═══════════════════════════════════════════════════════════════╝");
+        System.out.println("Press any key to continue: ");
+        InputHandler.stringHandler();
         defaultView.appContent();
         System.out.println("Thank you for using MOBLIMA!");
     }
