9ddf4342d00efceb3d10b13ad15d5252bd8a21b2
perfectsquare123 <91461666+perfectsquare123@users.noreply.github.com>
2022-11-09 22:23:15 +0800

    Javadoc for controller

--- src/controller/PaymentManager.java
+++ src/controller/PaymentManager.java
@@ -8,12 +8,28 @@ import java.util.*;
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime; 
 
+/**
+ * Controller for handling all logic related to Payments
+ * 
+ * @author Ci Hui
+ * @version 1.0
+ */
 
 public class PaymentManager {
     
+    /**
+     * Constructor
+     */
     public PaymentManager() {}
 
-
+    /**
+     * Method to create a {@link Payment} instance with Payment Type of QR Code
+     * and save to database
+     * 
+     * @param transactionID     is the transaction ID of Payment generated when payment is made by Movie Goer
+     * @param movieTicketPrice  is the total price of the Movie Tickets booked by Movie Goer
+     * @return                  the created {@link Payment} instance
+     */
     public static Payment createQRCodePayment(String transactionID, double movieTicketPrice) {
         String UUID = String.format("PY%04d", DatabaseHandler.generateUUID(Database.PAYMENT));
         Payment payment = new QRCodePayment(UUID, PaymentType.QRCODE, transactionID, movieTicketPrice);
@@ -21,7 +37,14 @@ public class PaymentManager {
         return payment;
     }
 
-
+    /**
+     * Method to create a {@link Payment} instance with Payment Type of Bank Transaction
+     * and save to database
+     * 
+     * @param transactionID     is the transaction ID of Payment generated when payment is made by Movie Goer
+     * @param movieTicketPrice  is the total price of the Movie Tickets booked by Movie Goer
+     * @return                  the created {@link Payment} instance
+     */
     public static Payment createBankTransactioPayment(String transactionID, double movieTicketPrice) {
         String UUID = String.format("PY%04d", DatabaseHandler.generateUUID(Database.PAYMENT));
         Payment payment = new BankTransactionPayment(UUID, PaymentType.BANK_TRANSACTION, transactionID, movieTicketPrice);
@@ -29,7 +52,14 @@ public class PaymentManager {
         return payment;
     }
 
-
+    /**
+     * Method to create a {@link Payment} instance with Payment Type of Card Payment
+     * and save to database
+     * 
+     * @param transactionID     is the transaction ID of Payment generated when payment is made by Movie Goer
+     * @param movieTicketPrice  is the total price of the Movie Tickets booked by Movie Goer
+     * @return                  the created {@link Payment} instance
+     */
     public static Payment createCardPayment(String transactionID, double movieTicketPrice) {
         String UUID = String.format("PY%04d", DatabaseHandler.generateUUID(Database.PAYMENT));
         Payment payment = new CardPayment(UUID, PaymentType.CARD_PAYMENT, transactionID, movieTicketPrice);
@@ -37,7 +67,16 @@ public class PaymentManager {
         return payment;
     }
 
-
+    /**
+     * Method to calculate the price of a Movie Ticket
+     * 
+     * @param movieGoer     is the {@link MovieGoer} who booked the Movie Ticket
+     * @param cinemaUUID    is the unique ID of the Cinema chosen by the Movie Goer
+     * @param movieUUID     is the unique ID of the Movie chosen by the Movie Goer
+     * @param seatUUID      is the unique ID of the Seat chosen by the Movie Goer
+     * @param showingTime   is the {@link DateTime} of Showing Time of the Movie chosen by the Movie Goer
+     * @return              the calculated price of the Movie Ticket
+     */
     public static double calculateMovieTicketPrice(MovieGoer movieGoer, String cinemaUUID, String movieUUID, String seatUUID, DateTime showingTime) {
         double totalPrice = 0;
 
@@ -54,15 +93,28 @@ public class PaymentManager {
         return totalPrice;
     }
 
-
+    /**
+     * Method to generate Transaction ID of the Payment
+     * Format of Transaction ID: XXXYYYYMMDDhhmm
+     * (XXX: Cinema Code in letters, YYYY: year, MM: month, DD: day, hh: hour, mm: minutes)
+     * 
+     * @param cinemaCode    is the Cinema Code of the chosen Cinema, 
+     *                      obtained from the last 3 characters of the Cinema UUID
+     * @return              the Transaction ID
+     */
     public static String generateTransactionId(String cinemaCode){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmm");  
         LocalDateTime now = LocalDateTime.now(); 
         String transactionId = cinemaCode + dtf.format(now);
         return transactionId;
     }
-
     
+    /**
+     * Method to obtain a Payment by its UUID
+     * 
+     * @param paymentUUID   is the target Payment's UUID
+     * @return              the target {@link Payment}
+     */
     public static Payment getPaymentByUUID(String paymentUUID) {
         ArrayList <Payment> payments = Database.getValueList(Database.PAYMENT.values());
 
@@ -75,7 +127,13 @@ public class PaymentManager {
         return null;
     }
 
-
+    /**
+     * Method to calculate the Price Weight when the day is a special day
+     * ie. Weekends or Holidays
+     * 
+     * @param showingTime   is the {@link DateTime} of the Showing Time of the Movie chosen by the Movie Goer
+     * @return              the Price Weight of Special Day (Weekend or Holiday)
+     */
     public static double calculateHolidayWeekendPrice(DateTime showingTime) {
         double weight = 1;
 
